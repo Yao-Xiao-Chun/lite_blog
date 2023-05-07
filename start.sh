68c6b2e3b9a6b113f981f8df54e794f41720ab09
GH <maguohui7@foxmail.com>
2023-05-07 19:16:35 +0800

    add:增加启动脚本

--- start.sh
+++ start.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 #开始构建程序
-echo "build:start"
+echo "build:tidy"
+#设置国内源
+export GOPROXY=https://proxy.golang.com.cn,direct
+#同步包
+/usr/local/go/bin/ go mod tidy
+pwd
+echo "start build"
+/usr/local/go/bin go build -o lite_blog ./main.go
+BUILD_ID=DONTKILLME
+echo "build:success"
+#检测服务是否存活
+if pgrep lite_blog > /dev/null
+then
+  killall lite_blog # kill lite_blog
+fi
+#启动服务
+chmod +x ./lite_blog
+nohup ./lite_blog >> lite_blog.log 2>&1 &
+echo "run lite_blog success"
